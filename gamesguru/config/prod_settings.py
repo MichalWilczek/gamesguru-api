d7145da106733dfde81ffd482d9fa41cec072cbc
MichalWilczek <mich.wilcz@gmail.com>
2023-11-05 18:02:12 +0100

    feat: add SSL certificates.

--- gamesguru/config/prod_settings.py
+++ gamesguru/config/prod_settings.py
@@ -3,3 +3,6 @@ CORS_ALLOWED_ORIGINS = [
    "https://gamesguru.pl",
 ]
 CSRF_TRUSTED_ORIGINS = ["https://gamesguru.pl*"]
+
+SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
