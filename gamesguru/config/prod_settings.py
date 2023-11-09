b452f150c0ff37e438b8d63072e1a1e846facd54
MichalWilczek <mich.wilcz@gmail.com>
2023-11-09 20:32:09 +0100

    feat: remove nginx and use caddy

--- gamesguru/config/prod_settings.py
+++ gamesguru/config/prod_settings.py
@@ -1,8 +1,5 @@
-ALLOWED_HOSTS = ['146.59.30.182']
+ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
-   "https://gamesguru.pl",
+   'https://gamesguru.pl',
 ]
-CSRF_TRUSTED_ORIGINS = ["https://gamesguru.pl*"]
-
-SECURE_SSL_REDIRECT = True
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = ['https://*gamesguru.pl']
--- proxy/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# fail the entire script if any of the command lines fail
-set -e
-
-# environment substitute
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
