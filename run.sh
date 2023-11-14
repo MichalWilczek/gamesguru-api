6ae6dd3c685a47b732d6c4187a8c4190f7a20832
MichalWilczek <mich.wilcz@gmail.com>
2023-11-14 18:31:20 +0100

    add uwsgi files and configs

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-gunicorn --config=/gunicorn.conf.py
+#gunicorn --config=/gunicorn.conf.py
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module gamesguru.wsgi
